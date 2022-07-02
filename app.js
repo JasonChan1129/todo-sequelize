8e8babc51898f7f23bae94f49d8ff5bdf5bf88a4
Jason Chan <jasonchan19981017@hotmail.com>
2022-07-02 23:43:17 +0800

    feat: register logic

--- app.js
+++ app.js
@@ -43,8 +43,29 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', async (req, res) => {
 	const { name, email, password, confirmPassword } = req.body;
-	const user = await User.create({ name, email, password });
-	return res.redirect('/');
+	User.findOne({ where: { email } }).then(user => {
+		if (user) {
+			console.log('User already exists');
+			return res.render('register', {
+				name,
+				email,
+				password,
+				confirmPassword,
+			});
+		}
+		return bcrypt
+			.genSalt(10)
+			.then(salt => bcrypt.hash(password, salt))
+			.then(hash =>
+				User.create({
+					name,
+					email,
+					password: hash,
+				})
+			)
+			.then(() => res.redirect('/'))
+			.catch(err => console.log(err));
+	});
 });
 
 app.get('/users/logout', (req, res) => {
